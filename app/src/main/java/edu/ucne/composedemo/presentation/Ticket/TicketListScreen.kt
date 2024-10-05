8c5cbbfc2a7a22f42934aaa44b6f89b87a3b971f
Enel Almonte <enel_almonte@ucne.edu.do>
2024-10-05 15:34:45 -0400

    Refactor TicketListScreen.kt to remove unused import and search icon

--- app/src/main/java/edu/ucne/composedemo/presentation/Ticket/TicketListScreen.kt
+++ app/src/main/java/edu/ucne/composedemo/presentation/Ticket/TicketListScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -43,7 +42,7 @@ fun TicketListScreen(
 
 @Composable
 fun TicketListBodyScreen(
-    uiState: UiState,
+    uiState: TicketUiState,
     goToTicket: (Int) -> Unit,
     createTicket: () -> Unit
 ) {
