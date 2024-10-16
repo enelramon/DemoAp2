538ce432cc72bfae269efb0b6c7391f2dbe18fe5
Eliezer Terrero <eliezerterrero12@gmail.com>
2024-10-16 20:28:18 -0300

    Creando SuplidorGastoDao

---
+++ app/src/main/java/edu/ucne/composedemo/data/local/dao/SuplidorGastoDao.kt
@@ -0,0 +1,16 @@
+package edu.ucne.composedemo.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.composedemo.data.local.entities.SuplidorGastoEntity
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface SuplidorGastoDao {
+    @Upsert
+    suspend fun save(suplidorGastoEntity: SuplidorGastoEntity)
+    @Query("Select * from suplidoresgastos")
+    suspend fun getSuplidoresGastos(): Flow<List<SuplidorGastoEntity>>
+}
