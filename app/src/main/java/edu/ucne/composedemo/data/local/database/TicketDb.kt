c7d9cbaffeaeddf6ccfb64a6a5cfa67e91970dac
Eliezer Terrero <eliezerterrero12@gmail.com>
2024-10-16 20:29:05 -0300

    Agregando la entidad a la base de datos

--- app/src/main/java/edu/ucne/composedemo/data/local/database/TicketDb.kt
+++ app/src/main/java/edu/ucne/composedemo/data/local/database/TicketDb.kt
@@ -2,16 +2,20 @@ package edu.ucne.composedemo.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import edu.ucne.composedemo.data.local.dao.SuplidorGastoDao
 import edu.ucne.composedemo.data.local.dao.TicketDao
+import edu.ucne.composedemo.data.local.entities.SuplidorGastoEntity
 import edu.ucne.composedemo.data.local.entities.TicketEntity
 
 @Database(
     entities = [
-        TicketEntity::class
+        TicketEntity::class,
+        SuplidorGastoEntity::class
     ],
     version = 1,
     exportSchema = false
 )
 abstract class TicketDb : RoomDatabase() {
     abstract fun ticketDao(): TicketDao
+    abstract fun suplidorGastoDao(): SuplidorGastoDao
 }
