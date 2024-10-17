15359e1a8f0f8d8cfdbea9c6eb4d1a8b0f15bb62
Luis Manuel <luis_duarte@ucne.edu.do>
2024-10-16 20:56:07 -0600

    Hice el CobroRepository

---
+++ app/src/main/java/edu/ucne/composedemo/data/repository/CobroRepository.kt
@@ -0,0 +1,31 @@
+package edu.ucne.composedemo.data.repository
+
+import edu.ucne.composedemo.data.remote.RemoteDataSource
+import edu.ucne.composedemo.data.remote.Resource
+import edu.ucne.composedemo.data.remote.dto.CobroDto
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import javax.inject.Inject
+
+class CobroRepository @Inject constructor(
+    private val remoteDataSource: RemoteDataSource
+){
+    suspend fun getCobro() : Flow<Resource<List<CobroDto>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val cobros =
+                remoteDataSource.getCobros()
+
+            emit(Resource.Success(cobros))
+        }catch (e : HttpException){
+            emit(Resource.Error("Error HTTP GENERAL ${e.message}"))
+        }catch (e: Exception){
+            emit(Resource.Error("Error Desconocido ${e.message}"))
+        }
+    }
+
+
+
+
+}
