0b59a49a67d4af321cd2a5287dce5dff2dc7f31e
Eliezer Terrero <eliezerterrero12@gmail.com>
2024-10-16 21:56:07 -0300

    Agregando EndPoints a la Api

--- app/src/main/java/edu/ucne/composedemo/data/remote/TicketingApi.kt
+++ app/src/main/java/edu/ucne/composedemo/data/remote/TicketingApi.kt
@@ -1,8 +1,12 @@
 package edu.ucne.composedemo.data.remote
 
+import edu.ucne.composedemo.data.local.entities.SuplidorGastoEntity
 import edu.ucne.composedemo.data.remote.dto.SistemaDto
+import edu.ucne.composedemo.data.remote.dto.SuplidorGastoDto
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
 import retrofit2.http.PUT
 
 interface TicketingApi {
@@ -11,5 +15,14 @@ interface TicketingApi {
 
     @PUT("api/Sistemas")
     suspend fun updateSistema(@Body sistemaDto: SistemaDto): SistemaDto
+
+    //SuplidoresGastos
+    @GET("api/SuplidoresGastos")
+    @Headers("X-API-Key","test")
+    suspend fun getSuplidoresGastos(): List<SuplidorGastoDto>
+
+    @Headers("X-API-Key","test")
+    @POST("api/SuplidoresGastos")
+    suspend fun addSuplidorGastos(@Body suplidorGastoDto: SuplidorGastoDto): SuplidorGastoDto
 
 }
