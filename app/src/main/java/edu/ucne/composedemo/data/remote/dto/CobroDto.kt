e95d0a21ac365d589739e54d9079dae4ba015320
Luis Manuel <luis_duarte@ucne.edu.do>
2024-10-16 20:55:57 -0600

    Hice el CobroDto

--- app/src/main/java/edu/ucne/composedemo/data/remote/dto/CobroDto.kt
+++ app/src/main/java/edu/ucne/composedemo/data/remote/dto/CobroDto.kt
@@ -1,10 +1,10 @@
 package edu.ucne.composedemo.data.remote.dto
 
-import java.util.Date
+import java.sql.Date
 
 data class CobroDto(
-     val idCobro: Int?,
-     val fecha: Date,
+     val idCobro: Int,
+     val fecha:Date,
      val codigoCliente: Int,
      val monto:Int,
      val observaciones: String
