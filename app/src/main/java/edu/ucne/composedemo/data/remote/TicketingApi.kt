cc04b9c87f19e8f2916af862fd57065d4d6ded12
Luis Manuel <luis_duarte@ucne.edu.do>
2024-10-16 20:57:04 -0600

    Agregue el get a el TicketingApi

--- app/src/main/java/edu/ucne/composedemo/data/remote/TicketingApi.kt
+++ app/src/main/java/edu/ucne/composedemo/data/remote/TicketingApi.kt
@@ -1,8 +1,10 @@
 package edu.ucne.composedemo.data.remote
 
+import edu.ucne.composedemo.data.remote.dto.CobroDto
 import edu.ucne.composedemo.data.remote.dto.SistemaDto
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.PUT
 
 interface TicketingApi {
@@ -11,5 +13,12 @@ interface TicketingApi {
 
     @PUT("api/Sistemas")
     suspend fun updateSistema(@Body sistemaDto: SistemaDto): SistemaDto
+
+    @GET("api/Cobros")
+    suspend fun  getCobro(): List<CobroDto>
+
+    @POST("api/Cobros")
+    suspend fun  postCobro(): List<CobroDto>
+
 
 }
