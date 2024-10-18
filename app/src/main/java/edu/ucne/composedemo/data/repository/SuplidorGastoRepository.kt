d9b7507dd83ad319c33371a0c79af0ccd841ea9c
Eliezer Terrero <eliezerterrero12@gmail.com>
2024-10-18 16:59:06 -0300

    Cambiando El repository de SuplidorGastos

--- app/src/main/java/edu/ucne/composedemo/data/repository/SuplidorGastoRepository.kt
+++ app/src/main/java/edu/ucne/composedemo/data/repository/SuplidorGastoRepository.kt
@@ -1,12 +1,9 @@
 package edu.ucne.composedemo.data.repository
 
-import edu.ucne.composedemo.data.local.dao.SuplidorGastoDao
-import edu.ucne.composedemo.data.local.entities.SuplidorGastoEntity
 import edu.ucne.composedemo.data.remote.RemoteDataSource
 import edu.ucne.composedemo.data.remote.Resource
-import edu.ucne.composedemo.data.remote.dto.toEntity
+import edu.ucne.composedemo.data.remote.dto.SuplidorGastoDto
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
 import javax.inject.Inject
@@ -14,26 +11,13 @@ import javax.inject.Inject
 
 class SuplidorGastoRepository @Inject constructor(
     private val remoteDataSource: RemoteDataSource,
-    private val suplidorGastoDao: SuplidorGastoDao
 ) {
-    private fun getSuplidoresGastosLocal() = suplidorGastoDao.getSuplidoresGastos()
-    private suspend fun saveSuplidorGastosLocal(suplidorGastoEntity: SuplidorGastoEntity) = suplidorGastoDao.save(suplidorGastoEntity)
+    fun getSuplidoresGastos(): Flow<Resource<List<SuplidorGastoDto>>> = flow {
 
-    fun getSuplidoresGastos(): Flow<Resource<List<SuplidorGastoEntity>>> = flow {
-
-        emit(Resource.Loading())
-        val suplidoresGastosLocal = getSuplidoresGastosLocal().firstOrNull()
-        if(suplidoresGastosLocal != null&& suplidoresGastosLocal.isEmpty()){
-            emit(Resource.Success(suplidoresGastosLocal))
-        }
         try {
             emit(Resource.Loading())
             val suplidorGastoRemote = remoteDataSource.getSuplidoresGastos()
-            suplidorGastoRemote.forEach{ suplidor ->
-                saveSuplidorGastosLocal(suplidor.toEntity())
-            }
-            val suplidoresGastosLocalUpdated = getSuplidoresGastosLocal().firstOrNull()
-            emit(Resource.Success(suplidoresGastosLocalUpdated?: emptyList()))
+            emit(Resource.Success(suplidorGastoRemote))
         }catch (e: HttpException){
             emit(Resource.Error("Error de internet"))
         }catch (e: Exception){
