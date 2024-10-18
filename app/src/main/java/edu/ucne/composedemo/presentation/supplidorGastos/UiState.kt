00e928b022bc3f0a6377fcccccc620c5fa963223
Eliezer Terrero <eliezerterrero12@gmail.com>
2024-10-18 16:58:40 -0300

    Cambiando la UiState

--- app/src/main/java/edu/ucne/composedemo/presentation/supplidorGastos/UiState.kt
+++ app/src/main/java/edu/ucne/composedemo/presentation/supplidorGastos/UiState.kt
@@ -1,33 +1,18 @@
 package edu.ucne.composedemo.presentation.supplidorGastos
 
-import edu.ucne.composedemo.data.local.entities.SuplidorGastoEntity
-import edu.ucne.composedemo.data.remote.dto.SistemaDto
 import edu.ucne.composedemo.data.remote.dto.SuplidorGastoDto
 
 data class UiState(
     val idSuplidor: Int? =null,
-    val nombres: String? =null,
-    val direccion: String? =null,
-    val telefono: String? =null,
-    val celular: String? =null,
-    val fax: String? =null,
-    val rnc: String? =null,
+    val nombres: String? ="",
+    val direccion: String? = "",
+    val telefono: String? ="",
+    val celular: String? ="",
+    val fax: String? ="",
+    val rnc: String? ="",
     val tipoNCF: Int? =null,
     val condicion: Int? =null,
-    val email: String? =null,
-    val suplidoresGastos: List<SuplidorGastoEntity> = emptyList(),
+    val email: String? ="",
+    val suplidoresGastos: List<SuplidorGastoDto> = emptyList(),
     val isLoading: Boolean = false
-)
-
-fun UiState.toEntity() = SuplidorGastoDto(
-    idSuplidor = idSuplidor,
-    nombres = nombres,
-    direccion = direccion,
-    telefono = telefono,
-    celular = celular,
-    fax = fax,
-    rnc = rnc,
-    tipoNCF = tipoNCF,
-    condicion = condicion,
-    email = email
 )
