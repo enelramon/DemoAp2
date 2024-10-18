b6190aa60fc1e548ea75904d6f102c07db74abd1
david-o-rosario1201 <finnnn023@gmail.com>
2024-10-17 21:25:42 -0400

    Agregando método GET de API EquiposAnyDesk

--- app/src/main/java/edu/ucne/composedemo/data/remote/TicketingApi.kt
+++ app/src/main/java/edu/ucne/composedemo/data/remote/TicketingApi.kt
@@ -1,5 +1,6 @@
 package edu.ucne.composedemo.data.remote
 
+import edu.ucne.composedemo.data.remote.dto.EquiposAnyDeskDto
 import edu.ucne.composedemo.data.remote.dto.SistemaDto
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -15,4 +16,8 @@ interface TicketingApi {
     @PUT("api/Sistemas")
     suspend fun updateSistema(@Body sistemaDto: SistemaDto): SistemaDto
 
+    //EquiposAnydesk
+    @Headers("X-API-Key:test")
+    @GET("api/EquiposAnydesk")
+    suspend fun getEquiposAnyDesks(): List<EquiposAnyDeskDto>
 }
