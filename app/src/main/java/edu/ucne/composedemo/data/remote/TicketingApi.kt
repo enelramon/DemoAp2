35014bcc176da637e38c9d1a361edc4436b12de3
Luis Manuel <luis_duarte@ucne.edu.do>
2024-10-17 20:43:44 -0600

    Agregue el GetCobros a la api

--- app/src/main/java/edu/ucne/composedemo/data/remote/TicketingApi.kt
+++ app/src/main/java/edu/ucne/composedemo/data/remote/TicketingApi.kt
@@ -1,6 +1,7 @@
 package edu.ucne.composedemo.data.remote
 
 import edu.ucne.composedemo.data.remote.dto.ClienteDto
+import edu.ucne.composedemo.data.remote.dto.CobroDto
 import edu.ucne.composedemo.data.remote.dto.EquiposAnyDeskDto
 import edu.ucne.composedemo.data.remote.dto.SistemaDto
 import retrofit2.http.Body
@@ -24,4 +25,9 @@ interface TicketingApi {
     @Headers("X-API-Key:test")
     @GET("api/EquiposAnydesk")
     suspend fun getEquiposAnyDesks(): List<EquiposAnyDeskDto>
+
+    @Headers("X-API-Key:test")
+    @GET("api/Cobros")
+    suspend fun getCobros(): List<CobroDto>
+
 }
