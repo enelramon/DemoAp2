082bffeaecdaf7bc37174d328d452fd7e0d595d0
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-10-20 18:52:33 -0400

    Agrega List de TiposSoportes

---
+++ app/src/main/java/edu/ucne/composedemo/presentation/tipossoportes/TiposSoportesList.kt
@@ -0,0 +1,126 @@
+package edu.ucne.composedemo.presentation.tipossoportes
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import edu.ucne.composedemo.data.remote.dto.TiposSoportesDto
+import edu.ucne.composedemo.presentation.components.TopBarComponent
+
+@Composable
+fun TiposSoportesListScreen(
+    viewModel: TiposSoportesViewModel = hiltViewModel(),
+    onDrawer: () -> Unit
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    TiposSoportesListBodyScreen(
+        uiState = uiState,
+        onDrawer = onDrawer
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TiposSoportesListBodyScreen(
+    uiState: TiposSoportesUiState,
+    onDrawer: () -> Unit
+) {
+    Scaffold(
+        Modifier.fillMaxSize(),
+        topBar = {
+            TopBarComponent (
+                title = "Tipos Soportes",
+                onDrawer
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            Spacer(
+                modifier = Modifier
+                    .height(32.dp)
+            )
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = "idTipo", modifier = Modifier.weight(0.10f))
+                Text(text = "Descripción", modifier = Modifier.weight(0.10f))
+                Text(text = "PrecioBase", modifier = Modifier.weight(0.10f))
+            }
+
+            when {
+                uiState.isLoading -> {
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                    )
+                }
+
+                uiState.errorMessage != null -> {
+                    Text(
+                        text = uiState.errorMessage,
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally),
+                        color = MaterialTheme.colorScheme.error
+                    )
+                }
+
+                else -> {
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxSize()
+                    ) {
+                        items(uiState.tiposSoportes) {
+                            TiposSoportesRow(it)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun TiposSoportesRow(
+    it: TiposSoportesDto
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(modifier = Modifier.weight(0.10f), text = it.idTipo.toString())
+            Text(modifier = Modifier.weight(0.10f), text = it.descripcion)
+            Text(modifier = Modifier.weight(0.10f), text = it.precioBase.toString())
+        }
+    }
+}
