dc86e2767de6e6444f85d0211dabddb8183205be
IsaacCoste <isaac24.coste@gmail.com>
2024-10-19 22:24:42 -0400

    Agregue el dto modificado

--- app/src/main/java/edu/ucne/composedemo/data/remote/dto/AnyDeskLogDto.kt
+++ app/src/main/java/edu/ucne/composedemo/data/remote/dto/AnyDeskLogDto.kt
@@ -4,13 +4,7 @@ import java.util.Date
 
 data class AnyDeskLogDto(
     val idCliente: Int?,
-    val cliente: String?,
-    val cantidad: Double?,
-    val minutos: Int?,
-    val fecha: Date?,
-    val origen: String?,
-    val destino: String?,
-    val desde: Date?,
-    val hasta: Date?,
-    val idEncargado: Int?,
+    val cliente: String,
+    val cantidad: Double,
+    val minutos: Int
 )
