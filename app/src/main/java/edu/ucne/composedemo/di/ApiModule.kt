78206c764f0779041d5fdeae56f36f987d860d49
IsaacCoste <isaac24.coste@gmail.com>
2024-10-20 00:13:01 -0400

    Cambios

--- app/src/main/java/edu/ucne/composedemo/di/ApiModule.kt
+++ app/src/main/java/edu/ucne/composedemo/di/ApiModule.kt
@@ -1,12 +1,16 @@
 package edu.ucne.composedemo.di
 
-
+import android.app.Application
+import android.content.Context
+import androidx.room.Room
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import edu.ucne.composedemo.data.local.database.TicketDb
 import edu.ucne.composedemo.data.remote.TicketingApi
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -22,7 +26,6 @@ object ApiModule {
     fun provideMoshi(): Moshi =
         Moshi.Builder()
             .add(KotlinJsonAdapterFactory())
-            .add(DateAdapter())
             .build()
 
     @Provides
@@ -30,17 +33,6 @@ object ApiModule {
     fun provideTicketingApi(moshi: Moshi): TicketingApi {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .build()
-            .create(TicketingApi::class.java)
-    }
-
-    const val BASE_URL2 = "https://anydesklog.azurewebsites.net/"
-    @Provides
-    @Singleton
-    fun provideAnyDeskLogApi(moshi: Moshi): TicketingApi {
-        return Retrofit.Builder()
-            .baseUrl(BASE_URL2)
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
             .create(TicketingApi::class.java)
