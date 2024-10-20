59f202c63d992d615f8478cbb9cf67eb0de4ad6e
IsaacCoste <isaac24.coste@gmail.com>
2024-10-19 22:30:11 -0400

    Agregue el repository utilizando flow

--- app/src/main/java/edu/ucne/composedemo/data/repository/AnyDeskLogRepository.kt
+++ app/src/main/java/edu/ucne/composedemo/data/repository/AnyDeskLogRepository.kt
@@ -6,23 +6,12 @@ import edu.ucne.composedemo.data.remote.dto.AnyDeskLogDto
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
+import javax.inject.Inject
 
-class AnyDeskLogRepository(
+class AnyDeskLogRepository @Inject constructor(
     private val remoteDataSource: RemoteDataSource
 ) {
-    fun getAllAnydeskLog(): Flow<Resource<List<AnyDeskLogDto>>> = flow {
-        try {
-            emit(Resource.Loading())
-            val anyDeskLog = remoteDataSource.getAllAnydeskLog()
-            emit(Resource.Success(anyDeskLog))
-        } catch (e: HttpException) {
-            emit(Resource.Error( "Error de Internetr: ${e.message}"))
-        }
-        catch (e: Exception) {
-            emit(Resource.Error("Error Desconocido: ${e.message}"))
-        }
-    }
-    fun getAnydeskLog(id: Int): Flow<Resource<AnyDeskLogDto>> = flow {
+    fun getAnydeskLog(id: Int): Flow<Resource<List<AnyDeskLogDto>>> = flow {
         try {
             emit(Resource.Loading())
             val anyDeskLog = remoteDataSource.getAnydeskLog(id)
