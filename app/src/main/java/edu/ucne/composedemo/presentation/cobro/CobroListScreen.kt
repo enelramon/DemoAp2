f55afab597508cf339f99d58d780c490d0a1af7c
Luis Manuel <luis_duarte@ucne.edu.do>
2024-10-20 19:26:14 -0600

    Resolvi Issue de Import Innecesario

--- app/src/main/java/edu/ucne/composedemo/presentation/cobro/CobroListScreen.kt
+++ app/src/main/java/edu/ucne/composedemo/presentation/cobro/CobroListScreen.kt
@@ -9,15 +9,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.material3.Card
+
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
+
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
+
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -29,7 +27,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import edu.ucne.composedemo.data.remote.dto.CobroDto
-import edu.ucne.composedemo.data.remote.dto.SistemaDto
+
 import java.text.SimpleDateFormat
 import java.util.Locale
 
