cfe53fa22f881b07c477ad97fabf6bae18587741
Aaron Hernandez <126830445+aaron-developer25@users.noreply.github.com>
2024-10-22 00:04:35 -0400

    Se agrega en el NavHost el acceso a la nueva navegacion

--- app/src/main/java/edu/ucne/composedemo/presentation/navigation/DemoAp2NavHost.kt
+++ app/src/main/java/edu/ucne/composedemo/presentation/navigation/DemoAp2NavHost.kt
@@ -13,6 +13,7 @@ import edu.ucne.composedemo.presentation.Ticket.TicketScreen
 import edu.ucne.composedemo.presentation.anydesklog.AnyDeskLogListScreen
 import edu.ucne.composedemo.presentation.cliente.ClienteListScreen
 import edu.ucne.composedemo.presentation.components.DrawerMenu
+import edu.ucne.composedemo.presentation.cxc.CxcListScreen
 import edu.ucne.composedemo.presentation.equiposanydesk.EquipoAnyDeskListScreen
 import edu.ucne.composedemo.presentation.sistema.SistemaListScreen
 import kotlinx.coroutines.launch
@@ -94,6 +95,15 @@ fun DemoAp2NavHost(
             }
             composable<Screen.AnyDeskLogList> {
                 AnyDeskLogListScreen(
+                    onDrawer = {
+                        scope.launch {
+                            drawerState.open()
+                        }
+                    }
+                )
+            }
+            composable<Screen.CxcList> {
+                CxcListScreen(
                     onDrawer = {
                         scope.launch {
                             drawerState.open()
