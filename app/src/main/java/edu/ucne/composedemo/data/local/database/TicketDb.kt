a388fee1605c24b8008cbea90c418e9872ccd651
Eliezer Terrero Rojas <eliezer_terrero@ucne.edu.do>
2024-10-22 15:11:36 -0400

    editando algunas cosas

--- app/src/main/java/edu/ucne/composedemo/data/local/database/TicketDb.kt
+++ app/src/main/java/edu/ucne/composedemo/data/local/database/TicketDb.kt
@@ -2,20 +2,16 @@ package edu.ucne.composedemo.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import edu.ucne.composedemo.data.local.dao.SuplidorGastoDao
 import edu.ucne.composedemo.data.local.dao.TicketDao
-import edu.ucne.composedemo.data.local.entities.SuplidorGastoEntity
 import edu.ucne.composedemo.data.local.entities.TicketEntity
 
 @Database(
     entities = [
         TicketEntity::class,
-        SuplidorGastoEntity::class
     ],
     version = 1,
     exportSchema = false
 )
 abstract class TicketDb : RoomDatabase() {
     abstract fun ticketDao(): TicketDao
-    abstract fun suplidorGastoDao(): SuplidorGastoDao
 }
