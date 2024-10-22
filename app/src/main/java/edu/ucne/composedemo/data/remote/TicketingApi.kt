ab189e789a856baa008d0bacf7793ced1ad487d0
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-10-22 11:59:00 -0400

    Agregando Endpoints de gastos a la api

--- app/src/main/java/edu/ucne/composedemo/data/remote/TicketingApi.kt
+++ app/src/main/java/edu/ucne/composedemo/data/remote/TicketingApi.kt
@@ -1,15 +1,25 @@
 package edu.ucne.composedemo.data.remote
 
+import edu.ucne.composedemo.data.remote.dto.GastoDto
 import edu.ucne.composedemo.data.remote.dto.SistemaDto
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.PUT
 
 interface TicketingApi {
+    @Headers("X-API-Key:test")
     @GET("api/Sistemas")
     suspend fun getSistemas(): List<SistemaDto>
 
+    @Headers("X-API-Key:test")
     @PUT("api/Sistemas")
     suspend fun updateSistema(@Body sistemaDto: SistemaDto): SistemaDto
+
+    @Headers("X-API-Key:test")
+    @GET("api/Gastos")
+    suspend fun getGastos(): List<GastoDto>
+
+
 
 }
