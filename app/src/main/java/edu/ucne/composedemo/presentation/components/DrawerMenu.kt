905ca7bda0552f4f0c46c2ed5833cdd51a7caf36
Aaron Hernandez <126830445+aaron-developer25@users.noreply.github.com>
2024-10-22 00:02:21 -0400

    Se agrega en el drawer la opcion en el menu

--- app/src/main/java/edu/ucne/composedemo/presentation/components/DrawerMenu.kt
+++ app/src/main/java/edu/ucne/composedemo/presentation/components/DrawerMenu.kt
@@ -60,6 +60,11 @@ fun DrawerMenu(
             title = "AnyDeskLogs",
             selectedIcon = Icons.Filled.Info,
             unselectedIcon = Icons.Outlined.Info
+        ),
+        NavigationItem(
+            title = "Cuentas x Cobrar",
+            selectedIcon = Icons.Filled.Info,
+            unselectedIcon = Icons.Outlined.Info
         )
     )
     val selectedItem = remember { mutableStateOf(items[0]) }
@@ -102,6 +107,7 @@ fun DrawerMenu(
                                 "Clientes" -> {navHostController.navigate(Screen.ClienteList)}
                                 "Equipos AnyDesks" -> {navHostController.navigate(Screen.EquiposAnyDeskList)}
                                 "AnyDeskLogs" -> {navHostController.navigate(Screen.AnyDeskLogList)}
+                                "Cuentas x Cobrar" -> {navHostController.navigate(Screen.CxcList)}
                             }
                         }
                     )
