eb047a4ce7342f259ff502ecc34feccf0637fad2
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-10-22 14:22:26 -0400

    Agregando Repository de Gastos

---
+++ app/src/main/java/edu/ucne/composedemo/data/repository/Gastosrepository.kt
@@ -0,0 +1,33 @@
+package edu.ucne.composedemo.data.repository
+
+import edu.ucne.composedemo.data.remote.RemoteDataSource
+import edu.ucne.composedemo.data.remote.Resource
+import edu.ucne.composedemo.data.remote.dto.GastoDto
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import javax.inject.Inject
+
+class Gastosrepository @Inject constructor(
+    private val remoteDataSource: RemoteDataSource
+){
+    fun getGastos(): Flow<Resource<List<GastoDto>>> = flow {
+
+        try{
+            emit(Resource.Loading())
+            val gastosApi = remoteDataSource.getGastos()
+            emit (Resource.Success(gastosApi))
+
+        }catch (e: HttpException){
+            emit(Resource.Error(e.message ?: "Error Desconocido"))
+
+        }
+        catch (e: Exception){
+            emit(Resource.Error(e.message ?: "Error Desconocido"))
+        }catch (e: Exception){
+            emit(Resource.Error(e.message ?: "Error Desconocido"))
+        }
+    }
+
+
+}
