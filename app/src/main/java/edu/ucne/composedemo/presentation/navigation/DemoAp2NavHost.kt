ab889664771f02cf26a391d8c0dc645bde06181b
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-10-22 12:02:26 -0400

    Agregando DateAdapter y la navegacion al NavHost

--- app/src/main/java/edu/ucne/composedemo/presentation/navigation/DemoAp2NavHost.kt
+++ app/src/main/java/edu/ucne/composedemo/presentation/navigation/DemoAp2NavHost.kt
@@ -1,21 +1,31 @@
 package edu.ucne.composedemo.presentation.navigation
 
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
 import edu.ucne.composedemo.presentation.Ticket.TicketListScreen
 import edu.ucne.composedemo.presentation.Ticket.TicketScreen
+import edu.ucne.composedemo.presentation.gastos.GastosListScreen
+import kotlinx.coroutines.launch
 
 
 @Composable
 fun DemoAp2NavHost(
-    navHostController: NavHostController
+    navHostController: NavHostController = rememberNavController()
+
 ) {
+    val scope = rememberCoroutineScope()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+
     NavHost(
         navController = navHostController,
-        startDestination = Screen.TicketList
+        startDestination = Screen.GastosList
     ) {
         composable<Screen.TicketList> {
             TicketListScreen(
@@ -37,5 +47,17 @@ fun DemoAp2NavHost(
                 }
             )
         }
+        composable<Screen.GastosList> {
+            GastosListScreen(
+                onDrawer = {
+                    scope.launch {
+                        drawerState.open()
+                    }
+                }
+
+            )
+        }
+
+
     }
 }
