f10c38137ae810f753037053c261f9962d387eb4
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-10-22 11:59:27 -0400

    Modificando RemoteDataSource

--- app/src/main/java/edu/ucne/composedemo/data/remote/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/composedemo/data/remote/RemoteDataSource.kt
@@ -1,6 +1,7 @@
 package edu.ucne.composedemo.data.remote
 
 import edu.ucne.composedemo.data.remote.dto.SistemaDto
+import edu.ucne.composedemo.data.remote.dto.GastoDto
 import javax.inject.Inject
 
 class RemoteDataSource @Inject constructor(
@@ -9,4 +10,9 @@ class RemoteDataSource @Inject constructor(
     suspend fun getSistemas() = ticketingApi.getSistemas()
 
     suspend fun updateSistemas(sistemaDto: SistemaDto) = ticketingApi.updateSistema(sistemaDto)
+
+    suspend fun getGastos() = ticketingApi.getGastos()
+
+
+
 }
