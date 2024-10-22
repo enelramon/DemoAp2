2b6eba2b1503abb376135ec8a3b73f9b4b93f49c
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-10-22 11:59:49 -0400

    Agregando NavigationItem

---
+++ app/src/main/java/edu/ucne/composedemo/NavigationItem.kt
@@ -0,0 +1,10 @@
+package edu.ucne.composedemo
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class NavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val badgeCount: Int? = null
+)
