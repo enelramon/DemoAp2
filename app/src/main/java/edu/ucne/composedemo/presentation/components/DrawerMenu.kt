27e4283e5551d67954e74f7b3dc958dd76471b89
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-10-22 12:33:26 -0400

    Agregando GastosList al DrawerMenu

--- app/src/main/java/edu/ucne/composedemo/presentation/components/DrawerMenu.kt
+++ app/src/main/java/edu/ucne/composedemo/presentation/components/DrawerMenu.kt
@@ -60,7 +60,13 @@ fun DrawerMenu(
             title = "AnyDeskLogs",
             selectedIcon = Icons.Filled.Info,
             unselectedIcon = Icons.Outlined.Info
+        ),
+        NavigationItem(
+            title = "Gastos",
+            selectedIcon = Icons.Filled.Info,
+            unselectedIcon = Icons.Outlined.Info
         )
+
     )
     val selectedItem = remember { mutableStateOf(items[0]) }
 
@@ -102,6 +108,8 @@ fun DrawerMenu(
                                 "Clientes" -> {navHostController.navigate(Screen.ClienteList)}
                                 "Equipos AnyDesks" -> {navHostController.navigate(Screen.EquiposAnyDeskList)}
                                 "AnyDeskLogs" -> {navHostController.navigate(Screen.AnyDeskLogList)}
+                                "Gastos" -> {navHostController.navigate(Screen.GastosList)}
+
                             }
                         }
                     )
