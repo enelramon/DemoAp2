1287a4d9bd5d1ba72253ee9b5bc47e92b7293ecd
ElPelfry <delfry_paulino@ucne.edu.do>
2024-10-26 20:22:07 -0400

    Se modifica DrawerItem para que no recibir una Instancia de la clase NavigationItem

--- app/src/main/java/edu/ucne/composedemo/presentation/navigation/DrawerItem.kt
+++ app/src/main/java/edu/ucne/composedemo/presentation/navigation/DrawerItem.kt
@@ -1,26 +1,43 @@
 package edu.ucne.composedemo.presentation.navigation
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material3.DrawerState
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import kotlinx.coroutines.launch
 
 @Composable
 fun DrawerItem(
-    item: NavigationItem,
-    isSelected: Boolean,
-    onClick: () -> Unit
+    title: String,
+    icon: ImageVector,
+    drawerState: DrawerState,
+    mutableState: MutableState<String>,
+    navegateTo: () -> Unit
 ) {
+    val scope = rememberCoroutineScope()
     NavigationDrawerItem(
         icon = {
             Icon(
-                imageVector = if (isSelected) item.selectedIcon
-                else item.unselectedIcon,
-                contentDescription = item.title
+                imageVector = icon,
+                contentDescription = title,
+                tint = if (title == mutableState.value)
+                    Color.Black else Color.Gray
             )
         },
-        label = { Text(text = item.title) },
-        selected = isSelected,
-        onClick = onClick
+        label = { Text(text = title) },
+        selected = mutableState.value == title,
+        onClick = {
+            navegateTo()
+            mutableState.value = title
+            scope.launch { drawerState.close() }
+        }
     )
 }
