97e8343c1455300f58f60beac12d6119fa6696a6
ElPelfry <delfry_paulino@ucne.edu.do>
2024-10-26 21:51:58 -0400

    Se remueve imports que no se utilizan

--- app/src/main/java/edu/ucne/composedemo/presentation/navigation/DrawerItem.kt
+++ app/src/main/java/edu/ucne/composedemo/presentation/navigation/DrawerItem.kt
@@ -1,18 +1,11 @@
 package edu.ucne.composedemo.presentation.navigation
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.outlined.Info
-import androidx.compose.material3.DrawerState
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import kotlinx.coroutines.launch
 
 @Composable
 fun DrawerItem(
