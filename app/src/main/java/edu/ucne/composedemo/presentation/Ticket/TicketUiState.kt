48e10f3eb1aeeffd16001a7136afbb7c8e0fb147
HenzoWTF <Urenahenzo@gmail.com>
2024-11-26 09:04:12 -0400

    Agrega las variables de Ticket al TicketUiState

--- app/src/main/java/edu/ucne/composedemo/presentation/Ticket/TicketUiState.kt
+++ app/src/main/java/edu/ucne/composedemo/presentation/Ticket/TicketUiState.kt
@@ -1,11 +1,30 @@
 package edu.ucne.composedemo.presentation.Ticket
 
-import edu.ucne.composedemo.data.local.entities.TicketEntity
+import edu.ucne.composedemo.data.remote.dto.ClienteDto
+import edu.ucne.composedemo.data.remote.dto.SistemaDto
+import edu.ucne.composedemo.data.remote.dto.TicketDto
 
 data class TicketUiState(
-    val ticketId: Int? = null,
-    val cliente: String? = "",
-    val asunto: String? = null,
-    val errorMessage: String? = null,
-    val tickets: List<TicketEntity> = emptyList()
-)
+    val idTicket: Double? = null,
+    val fecha: String? = "",
+    val vence: String? = "",
+    val idCliente: Double? = null,
+    val nombreClienteCompleto: String? = "",
+    val empresa: String? = "",
+    val solicitadoPor: String? = "",
+    val asunto: String? = "",
+    val prioridad: Int? = null,
+    val idEncargado: Int? = null,
+    val estatus: Int? = null,
+    val especificaciones: String? = "",
+    val archivo: String? = "",
+    val isSuccess: Boolean = false,
+    val isLoading: Boolean = false,
+    val errorCargar: String? = "",
+    val errorMessage: String? = "",
+    val nombreCliente: String = "",
+    val tickets: List<TicketDto> = emptyList(),
+    val clientes: List<ClienteDto> = emptyList(),
+    val sistema: List<SistemaDto> = emptyList(),
+
+    )
