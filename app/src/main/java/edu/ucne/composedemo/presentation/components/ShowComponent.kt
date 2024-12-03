83b7747236b8866c8f10021e87e400d8e47298d7
ElPelfry <delfry_paulino@ucne.edu.do>
2024-12-02 23:39:05 -0400

    renombre de ShowComponent

--- app/src/main/java/edu/ucne/composedemo/presentation/components/ShowComponent.kt
+++ app/src/main/java/edu/ucne/composedemo/presentation/components/ShowComponent.kt
@@ -3,7 +3,7 @@ package edu.ucne.composedemo.presentation.components
 import androidx.compose.runtime.Composable
 
 @Composable
-fun Showcomponent(
+fun ShowComponent(
     value: Boolean,
     whenContentIsTrue: @Composable () -> Unit = {},
     whenContentIsFalse: @Composable () -> Unit = {}
