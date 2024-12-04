cd7ac4b778c7aca6454cda23af2b81394af39681
david-o-rosario1201 <finnnn023@gmail.com>
2024-12-04 11:45:51 -0400

    Crea el test de TicketMetasRepository

---
+++ app/src/test/java/edu/ucne/composedemo/data/repository_tests/TicketMetaRepositoryTest.kt
@@ -0,0 +1,45 @@
+package edu.ucne.composedemo.data.repository_tests
+
+import app.cash.turbine.test
+import com.google.common.truth.Truth
+import edu.ucne.composedemo.data.remote.RemoteDataSource
+import edu.ucne.composedemo.data.remote.Resource
+import edu.ucne.composedemo.data.remote.dto.TicketMetaDto
+import edu.ucne.composedemo.data.repository.TicketMetaRepository
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+class TicketMetaRepositoryTest {
+
+    @Test
+    fun `should return a flow of ticketmetas`() = runTest {
+        //Given
+        val idUsuario = 1
+
+        val ticketmetas = listOf(
+            TicketMetaDto(
+                id = 1
+            ),
+            TicketMetaDto(
+                id = 2
+            )
+        )
+
+        val remoteDataSource = mockk<RemoteDataSource>()
+        val repository = TicketMetaRepository(remoteDataSource)
+
+        coEvery { remoteDataSource.getMetasUsuario(idUsuario) } returns ticketmetas
+
+        //When
+        repository.getMetasUsuario(idUsuario).test {
+            //Then
+            Truth.assertThat(awaitItem() is Resource.Loading).isTrue()
+
+            Truth.assertThat(awaitItem().data).isEqualTo(ticketmetas)
+
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+}
