3cc68be68812ae43d61ee6822989361146627356
david-o-rosario1201 <finnnn023@gmail.com>
2024-12-04 12:28:23 -0400

    Cambia el tipo de retorno de TicketMetaRequestDto a TicketMetaResponse

--- app/src/main/java/edu/ucne/composedemo/data/repository/TicketMetaRepository.kt
+++ app/src/main/java/edu/ucne/composedemo/data/repository/TicketMetaRepository.kt
@@ -3,6 +3,7 @@ package edu.ucne.composedemo.data.repository
 import edu.ucne.composedemo.data.remote.RemoteDataSource
 import edu.ucne.composedemo.data.remote.Resource
 import edu.ucne.composedemo.data.remote.dto.TicketMetaRequestDto
+import edu.ucne.composedemo.data.remote.dto.TicketMetaResponseDto
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
@@ -11,7 +12,7 @@ import javax.inject.Inject
 class TicketMetaRepository @Inject constructor(
     private val remoteDataSource: RemoteDataSource
 ) {
-    fun getMetasUsuario(idUsuario: Int): Flow<Resource<List<TicketMetaRequestDto>>> = flow {
+    fun getMetasUsuario(idUsuario: Int): Flow<Resource<List<TicketMetaResponseDto>>> = flow {
         try{
             emit(Resource.Loading())
             val metasUsuario = remoteDataSource.getMetasUsuario(idUsuario)
