448c1364f5bf0d3e4b460bb9d17f6cbf86cd0e35
david-o-rosario1201 <finnnn023@gmail.com>
2024-12-11 18:51:34 -0400

    Cambia el nombre de la variable idTicket a idUsuario

--- app/src/main/java/edu/ucne/composedemo/presentation/meta/TicketMetaUiEvent.kt
+++ app/src/main/java/edu/ucne/composedemo/presentation/meta/TicketMetaUiEvent.kt
@@ -1,7 +1,7 @@
 package edu.ucne.composedemo.presentation.meta
 
 sealed interface TicketMetaUiEvent {
-    data class SelectedTicketMeta(val idTicket: Int): TicketMetaUiEvent
+    data class SelectedTicketMeta(val idUsuario: Int): TicketMetaUiEvent
     data class Save(val idTicket: Int): TicketMetaUiEvent
     data object Delete: TicketMetaUiEvent
 }
