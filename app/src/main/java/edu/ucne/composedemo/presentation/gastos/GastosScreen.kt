ab6de3c00040c02cff2d1670738f9ecc9765c1d8
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-12-11 23:30:27 -0400

    Modifica llamadas a funciones por acceso indexado para mejorar la legibilidad

--- app/src/main/java/edu/ucne/composedemo/presentation/gastos/GastosScreen.kt
+++ app/src/main/java/edu/ucne/composedemo/presentation/gastos/GastosScreen.kt
@@ -57,9 +57,9 @@ fun GastosScreen(
 
     if (showDatePicker.value) {
         val calendar = Calendar.getInstance()
-        val year = calendar.get(Calendar.YEAR)
-        val month = calendar.get(Calendar.MONTH)
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val year = calendar[Calendar.YEAR]
+        val month = calendar[Calendar.MONTH]
+        val day = calendar[Calendar.DAY_OF_MONTH]
 
         DatePickerDialog(
             context,
