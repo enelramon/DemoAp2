faa58ddab85df14194f33994d0b630f507ec3839
Enel Almonte <enel_almonte@ucne.edu.do>
2025-02-12 21:13:38 -0400

    Agrega ejemplo de bottom sheet y filtro de fecha

--- app/src/main/java/edu/ucne/composedemo/presentation/Ticket/TicketListScreen.kt
+++ app/src/main/java/edu/ucne/composedemo/presentation/Ticket/TicketListScreen.kt
@@ -1,5 +1,7 @@
 package edu.ucne.composedemo.presentation.Ticket
 
+import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -15,22 +17,40 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Flag
 import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.Tune
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DatePickerDefaults
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DateRangePicker
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberDateRangePickerState
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -44,6 +64,7 @@ import edu.ucne.composedemo.presentation.components.TopBarComponent
 import edu.ucne.composedemo.presentation.meta.TicketMetaUiEvent
 import edu.ucne.composedemo.presentation.meta.TicketMetaViewModel
 import edu.ucne.composedemo.ui.theme.DemoAp2Theme
+import kotlinx.coroutines.launch
 
 @Composable
 fun TicketListScreen(
@@ -65,6 +86,7 @@ fun TicketListScreen(
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TicketListBodyScreen(
     uiState: TicketUiState,
@@ -74,6 +96,9 @@ fun TicketListBodyScreen(
     onDrawer: () -> Unit,
     goToMeta: (Int) -> Unit
 ) {
+
+    val openDialog = rememberSaveable { mutableStateOf(false) }
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
@@ -102,11 +127,22 @@ fun TicketListBodyScreen(
 
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally
-            ){Spacer(modifier = Modifier.height(32.dp))
+            ) {
+                Spacer(modifier = Modifier.height(32.dp))
 
                 Row(
                     verticalAlignment = Alignment.CenterVertically
-                ){
+                ) {
+                    IconButton(
+                        onClick = {
+                            openDialog.value = true
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Tune,
+                            contentDescription = "Tune"
+                        )
+                    }
                     RadioButton(
                         selected = false,
                         onClick = {}
@@ -190,8 +226,39 @@ fun TicketListBodyScreen(
             }
         }
     }
+
+    if (openDialog.value) {
+        val datePickerState = rememberDateRangePickerState()
+        DatePickerDialog(
+            onDismissRequest = {
+                // Dismiss the dialog when the user clicks outside the dialog or on the back
+                // button. If you want to disable that functionality, simply use an empty
+                // onDismissRequest.
+                openDialog.value = false
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        val range =
+                            datePickerState.selectedStartDateMillis!!..datePickerState.selectedEndDateMillis!!
+                        Log.d("DateRangePicker", "Selected range: $range")
+                        openDialog.value = false
+                    },
+                    enabled = datePickerState.selectedEndDateMillis != null
+                ) {
+                    Text(text = "Filtrar")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = { openDialog.value = false }) { Text("Cancel") }
+            }
+        ) {
+            DateRangePicker(state = datePickerState, modifier = Modifier.weight(1f))
+        }
+    }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TicketCard(
     ticket: TicketDto,
@@ -200,6 +267,11 @@ fun TicketCard(
     onTicketMetaEvent: (TicketMetaUiEvent) -> Unit = {},
     showAddMetaButton: Boolean = false
 ) {
+    var openBottomSheet by rememberSaveable { mutableStateOf(false) }
+    var skipPartiallyExpanded by rememberSaveable { mutableStateOf(false) }
+    val scope = rememberCoroutineScope()
+    val bottomSheetState =
+        rememberModalBottomSheetState(skipPartiallyExpanded = skipPartiallyExpanded)
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -222,7 +294,7 @@ fun TicketCard(
                     Text(text = "Ticket #: ${ticket.idTicket}")
                     Text(text = "Date: $date")
                 }
-                if(showAddMetaButton){
+                if (showAddMetaButton) {
                     Button(
                         onClick = {
                             onTicketMetaEvent(TicketMetaUiEvent.Save(ticket.idTicket ?: 0))
@@ -248,10 +320,57 @@ fun TicketCard(
                         )
                     }
                 }
+                IconButton(
+                    onClick = {
+                        openBottomSheet = !openBottomSheet
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.MoreVert,
+                        contentDescription = "Menu"
+                    )
+                }
             }
             Spacer(modifier = Modifier.height(4.dp))
             Text(text = "Empresa: ${ticket.idTicket}")
             Text(text = "Asunto: ${ticket.asunto}")
+        }
+
+    }
+    // Sheet content
+    if (openBottomSheet) {
+
+        ModalBottomSheet(
+            onDismissRequest = { openBottomSheet = false },
+            sheetState = bottomSheetState,
+        ) {
+            Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+                Button(
+                    // Note: If you provide logic outside of onDismissRequest to remove the sheet,
+                    // you must additionally handle intended state cleanup, if any.
+                    onClick = {
+                        scope
+                            .launch { bottomSheetState.hide() }
+                            .invokeOnCompletion {
+                                if (!bottomSheetState.isVisible) {
+                                    openBottomSheet = false
+                                }
+                            }
+                    }
+                ) {
+                    Text("Hide Bottom Sheet")
+                }
+            }
+            OutlinedButton(
+                onClick = {}
+            ) {
+                Text("Agregar a la meta")
+            }
+            OutlinedButton(
+                onClick = {}
+            ) {
+                Text("Eliminar ticket")
+            }
         }
     }
 }
