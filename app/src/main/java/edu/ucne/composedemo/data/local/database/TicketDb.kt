19231d873f1d95fc5471aba21422aebeacf55b58
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2025-03-19 04:14:02 -0400

    Agregando Deposito en la base de datos

--- app/src/main/java/edu/ucne/composedemo/data/local/database/TicketDb.kt
+++ app/src/main/java/edu/ucne/composedemo/data/local/database/TicketDb.kt
@@ -2,16 +2,20 @@ package edu.ucne.composedemo.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import edu.ucne.composedemo.data.local.dao.DepositoDao
 import edu.ucne.composedemo.data.local.dao.TicketDao
+import edu.ucne.composedemo.data.local.entities.DepositoEntity
 import edu.ucne.composedemo.data.local.entities.TicketEntity
 
 @Database(
     entities = [
         TicketEntity::class,
+        DepositoEntity::class
     ],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 abstract class TicketDb : RoomDatabase() {
     abstract fun ticketDao(): TicketDao
+    abstract fun depositoDao(): DepositoDao
 }
