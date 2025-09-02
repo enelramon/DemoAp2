532379612dd67cfd6bb7772e7b8c9a9bc0e29e1a
Yohualkis <Yohualkis99@gmail.com>
2025-09-02 12:41:15 -0400

    Add TareaDao for TareaEntity CRUD operations
    This commit introduces a Room DAO (Data Access Object) named `TareaDao`.
    
    This DAO provides the following functionalities for `TareaEntity`:
    - `save(tarea: TareaEntity)`: Upserts (inserts or updates) a `TareaEntity`.
    - `find(id: Int?)`: Retrieves a single `TareaEntity` by its `tareaId`.
    - `delete(tarea: TareaEntity)`: Deletes a `TareaEntity`.
    - `getAll()`: Returns a Flow of all `TareaEntity` objects.

---
+++ app/src/main/java/edu/ucne/composedemo/data/local/dao/TareaDao.kt
@@ -0,0 +1,23 @@
+package edu.ucne.composedemo.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.composedemo.data.local.entities.TareaEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+sealed interface TareaDao {
+    @Upsert
+    suspend fun save(tarea: TareaEntity)
+
+    @Query("select * from Tareas where tareaId = :id limit 1")
+    suspend fun find(id: Int?): TareaEntity
+
+    @Delete
+    suspend fun delete(tarea: TareaEntity)
+
+    @Query("select * from Tareas")
+    fun getAll(): Flow<List<TareaEntity>>
+}
