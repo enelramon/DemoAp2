61109fa9540e16f16ae486d7c6bbb8baee68c3db
Yohualkis <Yohualkis99@gmail.com>
2025-09-02 12:51:17 -0400

    Refactor tests to use runTest and remove unused imports

--- app/src/main/java/edu/ucne/composedemo/presentation/suplidorGastos/SuplidorGastosListScreen.kt
+++ app/src/main/java/edu/ucne/composedemo/presentation/suplidorGastos/SuplidorGastosListScreen.kt
@@ -20,7 +20,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
