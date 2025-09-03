3d7a10316b9fdf12a5267cf1d7a8292d9d5a6621
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-09-03 15:28:00 -0400

    MainActivity.kt llame tareaviewmodel

--- app/src/main/java/edu/ucne/composedemo/MainActivity.kt
+++ app/src/main/java/edu/ucne/composedemo/MainActivity.kt
@@ -4,9 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.composedemo.presentation.navigation.DemoAp2NavHost
+import edu.ucne.composedemo.presentation.tareas.TareaviewModel
 import edu.ucne.composedemo.ui.theme.DemoAp2Theme
 
 @AndroidEntryPoint
@@ -16,11 +22,19 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DemoAp2Theme {
-                val navHost = rememberNavController()
-                DemoAp2NavHost(navHost)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navHostController = rememberNavController()
+                    val tareaViewModel: TareaviewModel = hiltViewModel()
+
+                    DemoAp2NavHost(
+                        navHostController = navHostController,
+                        tareaViewModel = tareaViewModel
+                    )
+                }
             }
         }
     }
 }
-
-
