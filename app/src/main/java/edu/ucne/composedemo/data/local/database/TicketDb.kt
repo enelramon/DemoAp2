4a6fd6da0c6cdbb269c297be034fe5d797835ce9
Reyx388 <reyphillduarte06@gmail.com>
2025-09-02 21:17:04 -0400

    add: agrega la entiedad de Tarea a la base de datos

--- app/src/main/java/edu/ucne/composedemo/data/local/database/TicketDb.kt
+++ app/src/main/java/edu/ucne/composedemo/data/local/database/TicketDb.kt
@@ -2,16 +2,21 @@ package edu.ucne.composedemo.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import edu.ucne.composedemo.data.local.dao.TareaDao
 import edu.ucne.composedemo.data.local.dao.TicketDao
 import edu.ucne.composedemo.data.local.entities.TicketEntity
+import  edu.ucne.composedemo.data.local.entities.TareaEntity
 
 @Database(
     entities = [
         TicketEntity::class,
+        TareaEntity::class
     ],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
+
 abstract class TicketDb : RoomDatabase() {
     abstract fun ticketDao(): TicketDao
+    abstract fun tareasDao(): TareaDao
 }
