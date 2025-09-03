4dc348911600ea319f65e01dba4ca8ce37d7ed5a
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-09-03 15:24:23 -0400

    TareaEntity.kt la primarykey automatico

---
+++ app/src/main/java/edu/ucne/composedemo/data/local/entities/TareaEntity.kt
@@ -0,0 +1,13 @@
+package edu.ucne.composedemo.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Tareas")
+data class TareaEntity(
+    @PrimaryKey(autoGenerate = true)
+    val tareaid: Int = 0,
+    val descripcion: String="",
+    val tiempo:  Int=0
+
+)
