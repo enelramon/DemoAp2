bf57bb785b958370349ce1a5aa9acbe11d7bd091
ahb647 <58379725+ahb647@users.noreply.github.com>
2026-01-18 15:18:58 -0400

    Refactorizando ListTaskUiEvent para incluir nuevos eventos y mejorar la estructura

--- app/src/main/java/edu/ucne/composedemo/presentation/tareas/list/ListTaskUiEvent.kt
+++ app/src/main/java/edu/ucne/composedemo/presentation/tareas/list/ListTaskUiEvent.kt
@@ -1,9 +1,11 @@
 package edu.ucne.composedemo.presentation.tareas.list
 
-sealed interface ListTaskUiEvent {
-    data object Load : ListTaskUiEvent
-    data class Delete(val id: Int) : ListTaskUiEvent
-    data object CreateNew : ListTaskUiEvent
-    data class Edit(val id: Int) : ListTaskUiEvent
-    data class ShowMessage(val message: String) : ListTaskUiEvent
+sealed class ListTaskUiEvent {
+    object Load : ListTaskUiEvent()
+    object Refresh : ListTaskUiEvent()
+    data class Delete(val id: Int) : ListTaskUiEvent()
+    data class ShowMessage(val message: String) : ListTaskUiEvent()
+    object ClearMessage : ListTaskUiEvent()
+    object CreateNew : ListTaskUiEvent()
+    data class Edit(val id: Int) : ListTaskUiEvent()
 }
